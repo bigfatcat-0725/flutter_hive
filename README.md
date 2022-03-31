f26c53d9779ff942ac7df73d37a27a29f57ad350
bigfatcat <bigfatcat0725@gmail.com>
2022-03-31 12:20:52 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_hive
+
+- Hive study(예정)
---
+++ android/app/src/main/kotlin/com/bigfatcat0725/flutter_hive/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bigfatcat0725.flutter_hive
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
